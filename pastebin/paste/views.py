cef22661303a1c049ab518832c6fb5e545c33ba9
StefanDodoloi <153545504+StefanDodoloi@users.noreply.github.com>
2024-04-15 11:14:05 +0300

    Add files via upload

--- pastebin/paste/views.py
+++ pastebin/paste/views.py
@@ -3,14 +3,13 @@ from .models import Post
 from .forms import PostForm
 
 def post_list_view(request):
-    posts = Post.objects.all()
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save()
             return redirect('post_list')
-    else:
-        form = PostForm()
+    posts = Post.objects.all()
+    form = PostForm()
     return render(request, 'post_list.html', {'form': form, 'posts': posts})
 
 def post_detail_view(request, post_id):
